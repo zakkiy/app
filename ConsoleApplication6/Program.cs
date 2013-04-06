b1f202f0b6d4d636450dbeac2c3771a6aaf477bf
zakkiy <miyazaki0514@gmail.com>
2013-04-06 21:25:16 +0900

    コミット

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -9,6 +9,38 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
+            exec();
+        }
+
+        static void exec()
+        {
+            // int nは、キーボードからの入力
+            var input = Console.ReadLine();
+            if (input != null)
+            {
+                var n = int.Parse(input);  // ユーザーの入力した整数を読み込む
+                var c = new Calculator();
+                c.calc(n, 0);
+                Console.WriteLine(c.count);
+                //Console.ReadLine();
+                exec();
+                //System.out.println(c.count);
+            }
         }
     }
+
+    class Calculator
+    {
+        public int count = 0;
+        public void calc(int n, int p)
+        {
+            count++;
+            if (p > n) return;
+            for (int i = 0; i < n; i++)
+            {
+                calc(n, p + 1);
+            }
+        }
+    }
+
 }
